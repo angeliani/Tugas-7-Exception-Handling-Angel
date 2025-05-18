50ed7b05a1ab5585beca37ea0be6512b54803171
angel <angeliani.junitaviana@gmail.com>
2025-05-18 19:51:13 +0700

    Delete src/salonbooking directory

--- src/salonbooking/InvalidTimeException.java
+++
@@ -1,8 +0,0 @@
-package salonbooking;
-
-// custom exception untuk waktu booking yang tidak valid
-public class InvalidTimeException extends Exception {
-    public InvalidTimeException(String message) {
-        super(message);
-    }
-}
